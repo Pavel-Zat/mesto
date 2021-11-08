b3910bbeb60f419985d0ffc8f47dc1a95172b289
Pavel Z <pzeffectus@yandex.ru>
2021-11-08 20:32:53 +0300

    20.32  08-11-21

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,33 @@ const jobInput = formElement.querySelector('.popup__text_type_job');
 const nameProfile = document.querySelector('.profile__info-title');
 const jobProfile = document.querySelector('.profile__info-subtitle');
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const openPopup = function () {
   popupElement.classList.add('popup_is-opened');
   nameInput.value = nameProfile.textContent;
@@ -30,3 +57,67 @@ function formSubmitHandler(evt) {
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+const elementsList = document.querySelector('.elements__list');
+const cardsImg = document.querySelector('.elements__item');
+const heartButton = document.querySelector('.elements__heart');
+const cardsTemplate = document.querySelector('.cards_template');
+
+// let editing = null;
+
+function main() {
+	initialCards.forEach((element) => {
+		renderItem(element);
+	})
+
+	formButton.addEventListener('click', handleSubmit);
+}
+
+function renderItem(text) {
+	//1. Создавать разметку
+
+	const htmlElement = itemTemplate.content.cloneNode(true);
+
+	//2. Заменять в разметке текст
+	htmlElement.querySelector('.item__text').innerText = text;
+
+	//2.5 Навесить события
+	setListeners(htmlElement);
+
+	//3. Вставлять разметку в наш dom
+	list.appendChild(htmlElement);
+
+}
+
+
+
+// недоделанный первый вариант
+// const imgs = document.querySelectorAll('.elements__item');
+
+// const imgsArray = Array.from(imgs); // такой вызов вернёт полноценный массив 
+
+
+// console.log('Hello World!');
+// console.log(imgsArray);
+// // console.log(imgsArray[0].src);
+
+// const imgSrc = imgsArray.map(function (imgs) {
+//   const imgLink = initialCards.map(function (element) {
+//   return element.link;
+//   });
+//   return imgs.src = imgLink ;
+// });
+// console.log(imgSrc) ;
+
+// const imgLink = initialCards.map(function (element) {
+//   return element.link;
+// });
+// console.log(imgLink) ;
+
+// imgSrc.forEach(function (imgs) {
+
+  
+//     imgs.src = imgLink;
+
+//   });
