e1eb486f73c1462d7afff7ad05e21bfec09b1da0
Pavel Zatulyviter <pzeffectus@yandex.ru>
2021-11-09 16:21:34 +0300

    16.21 08-11-21

--- scripts/index.js
+++ scripts/index.js
@@ -7,32 +7,6 @@ const jobInput = formElement.querySelector('.popup__text_type_job');
 const nameProfile = document.querySelector('.profile__info-title');
 const jobProfile = document.querySelector('.profile__info-subtitle');
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 const openPopup = function () {
   popupElement.classList.add('popup_is-opened');
@@ -59,65 +33,63 @@ popupCloseButtonElement.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
 
-const elementsList = document.querySelector('.elements__list');
-const cardsImg = document.querySelector('.elements__item');
-const heartButton = document.querySelector('.elements__heart');
-const cardsTemplate = document.querySelector('.cards_template');
+const elementBlock = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#card-template').content;
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+initialCards.forEach((element) => {
+    renderCard(createCard(element.link, element.name));
+})
 
-// let editing = null;
-
-function main() {
-	initialCards.forEach((element) => {
-		renderItem(element);
-	})
-
-	formButton.addEventListener('click', handleSubmit);
+function createCard(srcValue, titleValue) {
+    const cardElement = cardTemplate.querySelector('.elements__list-item').cloneNode(true);
+    const cardImage = cardElement.querySelector('.elements__item');
+    initialCards.link = srcValue;
+    initialCards.name = titleValue;
+    cardElement.querySelector('.elements__text').textContent = titleValue;
+    cardElement.querySelector('.elements__heart').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('elements__heart_is-black');
+    });
+    cardElement.querySelector('.elements__trash').addEventListener('click', (evt) => {
+        const card = evt.target.closest('.elements__list-item');
+        card.remove();
+    });
+    cardImage.addEventListener('click', imagePopup);
+    return cardElement;
+}
+function renderCard(element) {
+    elementBlock.prepend(createCard());
 }
 
-function renderItem(text) {
-	//1. Создавать разметку
-
-	const htmlElement = itemTemplate.content.cloneNode(true);
-
-	//2. Заменять в разметке текст
-	htmlElement.querySelector('.item__text').innerText = text;
-
-	//2.5 Навесить события
-	setListeners(htmlElement);
-
-	//3. Вставлять разметку в наш dom
-	list.appendChild(htmlElement);
-
+function imagePopup(evt) {
+    const srcValue = evt.target.src;
+    const caption = evt.target.alt;
+    popupImage.querySelector('.popup__image').src = srcValue;
+    popupImage.querySelector('.popup__image').alt = caption;
+    popupImage.querySelector('.popup__caption').textContent = caption;
+    openPopup(popupImage);
 }
-
-
-
-// недоделанный первый вариант
-// const imgs = document.querySelectorAll('.elements__item');
-
-// const imgsArray = Array.from(imgs); // такой вызов вернёт полноценный массив 
-
-
-// console.log('Hello World!');
-// console.log(imgsArray);
-// // console.log(imgsArray[0].src);
-
-// const imgSrc = imgsArray.map(function (imgs) {
-//   const imgLink = initialCards.map(function (element) {
-//   return element.link;
-//   });
-//   return imgs.src = imgLink ;
-// });
-// console.log(imgSrc) ;
-
-// const imgLink = initialCards.map(function (element) {
-//   return element.link;
-// });
-// console.log(imgLink) ;
-
-// imgSrc.forEach(function (imgs) {
-
-  
-//     imgs.src = imgLink;
-
-//   });
