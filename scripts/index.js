39313fc146351ef1cb31fb1f1ddbb79d8a4384b5
Pavel Zatulyviter <pzeffectus@yandex.ru>
2021-11-16 13:43:21 +0300

    images are rendering 16-11-21

--- scripts/index.js
+++ scripts/index.js
@@ -57,36 +57,11 @@ popupAddCloseButtonElement.addEventListener('click', closePopupAdd);
 
 
 //переменные для 
-const cardsElements = document.querySelector('.elements');
+const cardsElements = document.querySelector('.elements__list');
 const cardTemplate = document.querySelector('.cards_template');
 const inputName = document.querySelector('.elements__text');
 const inputLink = document.querySelector('.elements__item');
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+
 
 //Функция, которая принимает в качестве аргумента данные карточки...
 function renderNewCard(element) {
