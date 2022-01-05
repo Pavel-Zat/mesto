63c0b9134647f5ec1ef7d9e36fc3e33ba08973ae
Pavel Z <pzeffectus@yandex.ru>
2022-01-05 18:25:55 +0300

    18.25 05-01-22

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,16 +3,39 @@
 
 
 
+function checkInputValidity(formElement, inputElement) { //функция проверки валидности инпута
+    if (!inputElement.validity.valid) {
+      //showInputError(formElement, inputElement, inputElement.validationMessage);
+      console.log('Инпут невалидный');
+    } else {
+      //hideInputError(formElement, inputElement);
+      console.log('Инпут валидный');
+    }
+  };
 
 
 
+function setInputListeners(formElement, {inputSelector}) {
+    const inputs = Array.from(formElement.querySelectorAll(inputSelector)); // выбираем инпуты в форме
+
+    inputs.forEach((input) => { //проходим по инпутам и добавляем событие 'input'
+        input.addEventListener('input', () => {
+                                //здесь проверяем валидность ввода + поведение кнопки
+            checkInputValidity(formElement, input);
+        })
+    })
+}
 
 
-
-
-const enableValidation = () => {
+function enableValidation({formSelector, ...rest}) { 
     // собрать все формы для валидации
-    const forms = document.querySelectorAll(.'form');
+    const forms = document.querySelectorAll(formSelector);
+    forms.forEach((formElement) => {
+        formElement.addEventListener('submit', (evt) => {
+            evt.preventDefault(); // отключили стандартное поведение для формы
+        });
+        setInputListeners(formElement, rest);
+    });
 }
 
 
@@ -20,8 +43,8 @@ const enableValidation = () => {
 // все настройки передаются при вызове
 
 enableValidation({
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
+    formSelector: '.form',
+    inputSelector: '.form__input',
     submitButtonSelector: '.popup__button',
     inactiveButtonClass: 'popup__button_disabled',
     inputErrorClass: 'popup__input_type_error',
