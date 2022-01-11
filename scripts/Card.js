f46d38d46a80cf4c9027fba862365a10461466d0
Pavel Z <pzeffectus@yandex.ru>
2022-01-11 22:24:24 +0300

    22.24 11-01-22

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,29 +1,41 @@
 class Card {
-    constructor //(cardSelector) {
- //     this._cardSelector = cardSelector;
- //   }
-  
-    _getTemplate() {
-//      const cardElement = document
- //       .querySelector(this._cardSelector)
-//        .content
-//        .querySelector('.card')
-//        .cloneNode(true);
-  
- //     return cardElement;
+    // принимает в конструктор её данные и селектор её template-элемента;
+    constructor(trash, item, text, heart) {
+        this._selector = selector;
+        this._trash = trash;
+        this._item = item;
+        this._text = text;
+        this._heart = heart;
+
     }
+    // содержит приватные методы, которые работают с разметкой, 
+    // устанавливают слушателей событий;
+	// содержит приватные методы для каждого обработчика;
+	// содержит один публичный метод, который возвращает полностью работоспособный и наполненный данными элемент карточки.
+    _getTemplate() {
+      const cardElement = document
+        //.querySelector(this._selector)
+        .querySelector('.elements__list-item')
+        .content
+        .querySelector('.cards')
+        .cloneNode(true);
   
-    _handleOpenPopup() {
+      return cardElement;
+    }
+
+    
+  
+//    _handleOpenPopup() {
 //      popupImage.src = this._image;
 //      popupElement.classList.add('popup_is-opened');
     }
   
-    _handleClosePopup() {
+//    _handleClosePopup() {
  //     popupImage.src = '';
 //      popupElement.classList.remove('popup_is-opened');
     }
   
-    _setEventListeners() {
+//    _setEventListeners() {
 //      this._element.addEventListener('click', () => {
 //        this._handleOpenPopup();
 //      });
@@ -34,3 +46,5 @@ class Card {
     }
   }
  export default Card; 
+
+ //Для каждой карточки создайте экземпляр класса Card.
