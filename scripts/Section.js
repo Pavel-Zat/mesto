da99b6e971591dc65a00e82d1d5d87c6d9025b19
Pavel Z <pzeffectus@yandex.ru>
2022-02-02 07:43:17 +0300

    7.28 02-02-22

---
+++ scripts/Section.js
@@ -0,0 +1,24 @@
+export default class Section {
+    constructor( { items, renderer }, containerSelector) {
+        this._renderedItems = items;
+        this._renderer = renderer; 
+    
+        this._container = document.querySelector(containerSelector); //DOM-элемент, найденный 
+  }
+
+  renderItems() { // перебирает массив данных _renderedItems. Вызывает
+    this.clear();
+
+    this._renderedItems.forEach(item => {
+        this._renderer(item);
+    })
+  }
+
+  clear() { // метод удаляет всё содержимое поля _container
+    this._container.innerHTML = '';
+  }
+
+  setItem(element) { //принимает параметр element и вставляет его
+    this._container.append(element);
+  }
+}
